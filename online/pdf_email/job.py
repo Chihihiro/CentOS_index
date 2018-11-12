d3441780fbd7e027eb7804d5623eb1c8da42fdee
Chihiro <632207812@qq.com>
2018-11-12 18:02:23 +0800

    word pass

--- online/pdf_email/job.py
+++ online/pdf_email/job.py
@@ -40,7 +40,29 @@ def printHello():
     t.start()
 
 
+def work():
+    time = now_time2()
+    num = len(len_pdf())
+    if 6 > time >= 22 and num > 50:
+        main()
+        # t = Timer(1*60*60, say_work)
+        # t.start()
+    elif time == 23:
+        if num == 0:
+            # t = Timer(1 * 60 * 60, say_work)
+            # t.start()
+            pass
+        else:
+            main()
+            # t = Timer(1*60*60, say_work)
+            # t.start()
+    else:
+        print("Hello World")
+        # t = Timer(1*60*60, say_work)
+        # t.start()
+
+
 if __name__ == "__main__":
-    say_work()
+    work()
 
 
