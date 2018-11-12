ea8d9faa73e5c66bddd965f4230ccf497d380df9
Chihiro <632207812@qq.com>
2018-11-12 18:08:01 +0800

    word pass

--- online/pdf_email/pdf_TorF.py
+++ online/pdf_email/pdf_TorF.py
@@ -503,6 +503,8 @@ def main():
     if len(lls) > 0:
         e = to_email('hkstock@gildata.com')
         e.course()
+        e1 = to_email('632207812@qq.com')
+        e1.course()
 
 
 if __name__ == '__main__':
