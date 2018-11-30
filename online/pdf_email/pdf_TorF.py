2af11921ef0595824a04c05c2de5f039fc234e88
Chihiro <632207812@qq.com>
2018-11-30 17:18:20 +0800

    word pass

--- online/pdf_email/pdf_TorF.py
+++ online/pdf_email/pdf_TorF.py
@@ -519,8 +519,8 @@ def main():
     if len(lls) > 0:
         e = to_email('hkstock@gildata.com')
         e.course()
-        # e1 = to_email('632207812@qq.com')
-        # e1.course()
+        e1 = to_email('632207812@qq.com')
+        e1.course()
 
 def work():
     time = now_num()
